2b65cced3040a8c72748b4e5c51ef5e8464902b9
Okezie <chekwube102@gmail.com>
2023-03-26 15:38:10 +0100

    FB intergration testing complete

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -4,59 +4,47 @@ const express = require('express'); //returns a function
 const app = express(); //Returns an object of type Express
 app.use(express.json()); //Allows our server to accept JSON parsing as a body in POST command or so.
 
-const serverless = require('serverless-http');
-
 const router = express.Router();
 app.use('/.netlify/functions/index', router);
 
-
-//module.exports.handler = async function (event, context) {
-    const bot_Import = require('./to_frontend/telegrafAPI');
-
-    const botMod = bot_Import.bot; //bot_Import.bot;
-    const bToken = bot_Import.botToken;
-    const _url = bot_Import.hookUrl;
-
-    console.log("Token:", bToken);
-    console.log("URL:", _url);
-
-    const repo = bot_Import.kBoards.daBase;
-
-    botMod.telegram.setWebhook(_url + bToken); // Run this once to connect the webhook.
-    router.use(botMod.webhookCallback("/" + bToken));
-    botMod.startWebhook("/" + bToken, null, null); //To start the webhook.
+const serverless = require('serverless-http');
 
 
-    router.get('/', async (req, res) => {
-        console.log("Welcome to this endpoint!");
-        res.send("Hello World, Welcome to my Lambda function.");
-    });
+const bot_Import = require('./to_frontend/telegrafAPI');
+const botMod = bot_Import.bot;
+const bToken = bot_Import.botToken;
+const _url = bot_Import.hookUrl;
 
-    router.get('/logs', async (req, res) => {
-        console.log("Logs endpoint!");
-        res.send(repo.dbLogs);
-    });
 
-    /*
-    router.get('/dbread', async (req, res) => {
-        console.log("DB endpoint!", "Reading DB...");
-        repo.testRetrieve();
-        res.send("Read complete");
-    });
+//botMod.telegram.setWebhook(_url + bToken); // Run this once to connect the webhook.
+router.use(botMod.webhookCallback("/" + bToken));
+botMod.startWebhook("/" + bToken, null, null); //To start the webhook.
 
-    router.get('/dbwrite', async (req, res) => {
-        console.log("DB endpoint!", "Writing to DB...");
-        repo.testSend();
-        res.send("Write complete");
-    });
 
-    
-    return {
-        statusCode: 200,
-        body: "Working"
-    }*/
+router.get('/', async (req, res) => {
+    console.log("Welcome to the main endpoint!");
+    res.send("Hello World, Welcome to my Lambda function endpoint.");
+});
 
-//}
+router.get('/logs', async (req, res) => {
+    console.log("Logs endpoint!");
+    res.send(repo.dbLogs);
+});
+
+/*
+const repo = bot_Import.kBoards.daBase;
+
+router.get('/dbread', async (req, res) => {
+    console.log("DB endpoint!", "Reading DB...");
+    repo.testRetrieve();
+    res.send("Read complete");
+});
+
+router.get('/dbwrite', async (req, res) => {
+    console.log("DB endpoint!", "Writing to DB...");
+    repo.testSend();
+    res.send("Write complete");
+});*/
 
 
 module.exports = app;
